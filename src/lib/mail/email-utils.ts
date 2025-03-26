f01cc394a98c6160612c7bc17c3845b1a75f4184
jdalmeida <joaogabrielde.almeida@hotmail.com>
2025-03-26 08:18:35 -0300

    mail fix

--- src/lib/mail/email-utils.ts
+++ src/lib/mail/email-utils.ts
@@ -1,5 +1,6 @@
 "use server"
 import nodemailer from 'nodemailer';
+import type SMTPPool from 'nodemailer/lib/smtp-pool';
 
 export async function sendEmail(
   to: string,
@@ -7,15 +8,17 @@ export async function sendEmail(
   htmlContent: string
 ): Promise<void> {
 
-  const transporter = nodemailer.createTransport({
+  const config: SMTPPool.Options = {
+    pool: true,
     host: process.env.MAILHOST,
-    port: process.env.MAILPORT??465,
+    port: Number(process.env.MAILPORT),
     secure: false,
     auth: {
       user: process.env.MAILUSER??'', // Seu e-mail
       pass: process.env.MAILPASS??'', // Sua senha
     },
-  });
+  }
+  const transporter = nodemailer.createTransport(config);
 
   // Configuração do e-mail
   const mailOptions = {
@@ -27,7 +30,7 @@ export async function sendEmail(
 
   // Envio do e-mail
   try {
-    const info = await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
     console.log('E-mail enviado para: %s', to);
   } catch (error) {
     console.error('Erro ao enviar e-mail: ', error);
