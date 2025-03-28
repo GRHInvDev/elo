44f3fb5a7d586dedf303b69da2bfd06639ef2b8f
jdalmeida <joaogabrielde.almeida@hotmail.com>
2025-03-28 17:51:15 -0300

    initial new dash

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next"
 
 import { DashboardShell } from "@/components/dashboard-shell"
-import { ContentFeed } from "@/components/content-feed"
-import { QuickAccess } from "@/components/quick-access"
-import { api } from "@/trpc/server"
-import { MonthlyBirthdays } from "@/components/monthly-birthdays"
+import { MainCarousel } from "@/components/dashboard/main-carousel"
+import { BirthdaysCarousel } from "@/components/dashboard/birthdays-carousel"
 
 export const metadata: Metadata = {
   title: "Dashboard | elo",
@@ -12,30 +10,31 @@ export const metadata: Metadata = {
 }
 
 export default async function DashboardPage() {
-  const me = await api.user.me();
-  const today = new Date();
-  
+
+
   return (
     <DashboardShell>
-      <div>
-        {me?.birthDay?.data && 
-          me?.birthDay?.data.getDate() === today.getDate() && 
-          me?.birthDay?.data.getMonth() === today.getMonth() ?
-          <div>
-            <h1 className='text-2xl font-bold'>Feliz aniversário {me.firstName}!! 🎉</h1>
-          </div>
-          :
-          <div>
-            <h1 className='text-2xl font-bold'>Olá {me?.firstName}!</h1>
-          </div>
-        }
-      </div>
-      <div className="flex flex-col-reverse md:grid gap-4 md:grid-cols-2 lg:grid-cols-7 w-full">
-        <ContentFeed className="col-span-5 w-full" />
-        <div className="col-span-2 space-y-4 w-full">
-          <QuickAccess className="w-full" />
-          <MonthlyBirthdays className="w-full"/>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <MainCarousel className="col-span-1 md:col-span-2" itens={[
+          {
+            imageRef: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc8.alamy.com%2Fcomp%2F2CA8TE1%2Fdesign-label-2CA8TE1.jpg&f=1&nofb=1&ipt=fc122be5b7acb593ce1a4c07149a912616e8d4af5515952924c6bd5de6c43726&ipo=images',
+            title: 'something'
+          },
+          {
+            imageRef: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc8.alamy.com%2Fcomp%2F2HD6CE3%2Fpresentation-templates-elements-template-design-for-print-banner-web-set-of-cover-header-brochure-2HD6CE3.jpg&f=1&nofb=1&ipt=e7f318f74d93debaa155704c1843c44f031c7fb48cc00f4c60f249b192caa80b&ipo=images',
+            title: '2'
+          },
+        ]}/>
+        <BirthdaysCarousel className="col-span-1 md:col-span-1" itens={[
+          {
+            imageRef: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc8.alamy.com%2Fcomp%2F2CA8TE1%2Fdesign-label-2CA8TE1.jpg&f=1&nofb=1&ipt=fc122be5b7acb593ce1a4c07149a912616e8d4af5515952924c6bd5de6c43726&ipo=images',
+            title: 'something'
+          },
+          {
+            imageRef: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc8.alamy.com%2Fcomp%2F2HD6CE3%2Fpresentation-templates-elements-template-design-for-print-banner-web-set-of-cover-header-brochure-2HD6CE3.jpg&f=1&nofb=1&ipt=e7f318f74d93debaa155704c1843c44f031c7fb48cc00f4c60f249b192caa80b&ipo=images',
+            title: '2'
+          },
+        ]}/>
       </div>
     </DashboardShell>
   )
