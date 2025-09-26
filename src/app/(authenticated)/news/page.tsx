53063652c33b61dc92bdbd1b3400552e60cb8f80
Ruan Bueno <rbcr4z1@gmail.com>
2025-09-26 09:21:15 -0300

    style: remove unnecessary class from DashboardShell in NewsPage

--- src/app/(authenticated)/news/page.tsx
+++ src/app/(authenticated)/news/page.tsx
@@ -3,7 +3,7 @@ import { DashboardShell } from "@/components/dashboard-shell";
 
 export default function NewsPage() {
   return (
-    <DashboardShell className="flex justify-center p-0 md:p-8">
+    <DashboardShell className="p-0 md:p-8">
       <ContentFeed className="w-full md:max-w-lg lg:max-w-xl"/>
     </DashboardShell>
   );
