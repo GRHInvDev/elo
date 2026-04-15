96b9779f49699f818bb970cd4b90dfd105e55cb7
Ruan Bueno <rbcr4z1@gmail.com>
2026-04-15 18:15:29 -0300

    refactor: remove unused suggestion creation functionality from the API

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -182,21 +182,6 @@ export const api = {
       return caller.aiAssistant.notifyColleague(input);
     },
   },
-  suggestion: {
-    create: async (input: {
-      submittedName?: string;
-      submittedSector?: string;
-      description: string;
-      problem?: string;
-      contribution: {
-        type: "IDEIA_INOVADORA" | "SUGESTAO_MELHORIA" | "SOLUCAO_PROBLEMA" | "OUTRO";
-        other?: string;
-      };
-    }) => {
-      const caller = await createServerCaller();
-      return caller.suggestion.create(input);
-    },
-  },
   foodOrder: {
     create: async (input: {
       restaurantId: string;
